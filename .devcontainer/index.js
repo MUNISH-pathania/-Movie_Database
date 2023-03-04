65218b421a51fd016cd9f58c48d0211e71ef5cda
MUNISH-pathania <110280635+MUNISH-pathania@users.noreply.github.com>
2023-03-04 16:28:51 +0000

    changes

--- .devcontainer/index.js
+++ .devcontainer/index.js
@@ -2,10 +2,12 @@ const movieCreation = document.querySelector('#Movie-creation');
 const movieretrieval = document.querySelector('#Movie-retrieval');
 const container = document.querySelector('.container');
 
+
 let currentCard = null; // keep track of the currently displayed card
 
 movieCreation.addEventListener('click', createM);
 movieretrieval.addEventListener('click', movieRetrival);
+
 
 
 
@@ -28,6 +30,8 @@ function createM() {
 
     movieForm.innerHTML = formHTML;
     movieForm.addEventListener('submit', btnOperation);
+
+    
 
    
 
@@ -69,6 +73,13 @@ movies.push(movie);
 
 // store the updated movies array in local storage
 localStorage.setItem('movies', JSON.stringify(movies));
+
+// clearing the input feild
+document.querySelector('.title').value = '';
+document.querySelector('.director').value = '';
+document.querySelector('.release').value = '';
+document.querySelector('.genre').value = '';
+
     };
 
 
@@ -80,17 +91,18 @@ function movieRetrival(){
     currentCard = null;
   }
   const moviediv = document.createElement('div');
-  moviediv.classList.add('movie-input','reterivalDiv' );
+  // moviediv.classList.add('reterivalDiv' );
   const movies = JSON.parse(localStorage.getItem('movies'));
 
   let movieHTML = '';
   movies.forEach((movie) => {
     movieHTML += `
-      <div>
-        <p>Title: ${movie.title}</p>
-        <p>Director: ${movie.director}</p>
-        <p>Year of Release: ${movie.yearOfRelease}</p>
-        <p>Genre: ${movie.genre}</p>
+      <div class="reterivalDiv">
+        <p class="movie-p">Title: ${movie.title} </p>
+        <p class="movie-p">Director: ${movie.director}</p>
+        <p class="movie-p">Year of Release: ${movie.yearOfRelease}</p>
+        <p class="movie-p">Genre: ${movie.genre}</p>
+        <p class="edit"><i class="fa fa-pencil-square-o" aria-hidden="true edit"></i></p>
       </div>
     `;
     // const movieElement = document.createElement('div');
@@ -100,9 +112,34 @@ function movieRetrival(){
 
     moviediv.innerHTML =movieHTML;
   });
-
-
 container.appendChild(moviediv);
  currentCard = moviediv;
 
+ // edit btn
+ let  editbtn = document.querySelector('.edit');
+ editbtn.addEventListener('click', editFn);
+
+ function editFn(){
+  let titleValue = document.querySelector('.title')
+
+    let directorValue = document.querySelector('.director')
+
+    let releaseValue = document.querySelector('.release')
+    let movieGenreValue = document.querySelector('.genre')
+
+    let  btnR = document.querySelector('.reterivalDiv');
+
+    btnR.remove();
+    createM();
+
+    let  movies = localStorage.getItem('movies');
+    movies.forEach((movie) => {
+          console.log(titleValue.value = movie[0].title); 
+    })
+
+
+
+
 }
+};
+
